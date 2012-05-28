79aa1fbfc2cd2de9e3adcc39bd8f104da73bdc7b
Ivajkin Timofej <timatnet@gmail.com>
2012-05-28 22:51:43 +1100

    Ready to start

--- server/start.js
+++ server/start.js
@@ -1,6 +1,12 @@
-var http = require('http');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
