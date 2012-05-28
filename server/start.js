1cf03db87ad4a2d697c602077c15681b29a0a5d8
Ivajkin Timofej <timatnet@gmail.com>
2012-05-28 23:27:17 +1100

    send

--- server/start.js
+++ server/start.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	fs.readFile('client/index.html', function (err, data) {
 		if (err) throw err;
-		response.send(data);
+		response.send('' + data);
 	});
 });
 
