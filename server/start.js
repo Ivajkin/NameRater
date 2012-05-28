f9b0f15a04732d6735bfb959065f41d52e53e09f
Ivajkin Timofej <timatnet@gmail.com>
2012-05-28 23:19:56 +1100

    Web server reading index.html

--- server/start.js
+++ server/start.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require('express'),
+	fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	fs.readFile('/console/index.html', function (err, data) {
+		if (err) throw err;
+		response.send(data);
+	});
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
